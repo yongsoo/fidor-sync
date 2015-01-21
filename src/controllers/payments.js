8c8fbb6166bf0f796f8c665c3e14108b0d940e5f
Yong-Soo Chung <yongsoochung@gmail.com>
2015-01-21 13:17:02 -0800

    [FEATURE] Controllers: Add create and show functionality in payments controller

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -3,13 +3,36 @@ module.exports = function(models, lib) {
 
   return {
     create: function(req, res, next) {
-      next(new Error('Unimplemented'))
+      models.Payment.generate({
+        amount: req.body.amount,
+        account_id: req.body.account_id,
+        external_uid: req.body.external_uid
+      })
+      .then(function(payment) {
+        res.status(200).send({
+          success: true,
+          payment: payment
+        })
+      })
+      .error(next)
     },
     show: function(req, res, next) {
-      next(new Error('Unimplemented'))
-    },
-    cancel: function(req, res, next) {
-      next(new Error('Unimplemented'))
+      new models.Payment({ external_uid: req.params.external_uid })
+        .fetch()
+        .then(function(payment) {
+          if (payment) {
+            res.status(200).send({
+              success: true,
+              payment: payment
+            })
+          } else {
+            next(new Error('Payment not found'));
+          }
+        })
+        .error(next)
     }
+    // cancel: function(req, res, next) {
+    //   next(new Error('Unimplemented'))
+    // }
   }
 }
