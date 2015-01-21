85374340c73cbe6bd1fddbff535b133a014a0bf4
Yong-Soo Chung <yongsoochung@gmail.com>
2015-01-20 17:49:45 -0800

    [FEATURE] Add getPayment function to fidor_client

--- src/lib/fidor_client.js
+++ src/lib/fidor_client.js
@@ -2,16 +2,17 @@ var Promise = require('bluebird');
 var http = require('superagent');
 
 function FidorClient(fidorOptions) {
-  this.url = fidorOptions.url
-  this.accessToken = fidorOptions.accessToken
-  this.accountId = fidorOptions.accountId
-  this.clientId = fidorOptions.clientId
-  this.clientSecret = fidorOptions.clientSecret
+  this.url = fidorOptions.url;
+  this.accessToken = fidorOptions.accessToken;
+  this.accountId = fidorOptions.accountId;
+  this.clientId = fidorOptions.clientId;
+  this.clientSecret = fidorOptions.clientSecret;
 }
 
 FidorClient.prototype = {
   sendPayment: function(paymentOptions) {
     var _this = this;
+
     return new Promise(resolve, reject) {
       var payment = {
         amount: paymentOptions.amount,
@@ -27,14 +28,30 @@ FidorClient.prototype = {
         .post(_this.url + '/sepa_credit_transfers')
         .set('Content-Type', 'application/json')
         .send(payment)
-        .end(function(error, paymentResponse) {
+        .end(function(error, response) {
           if (error) {
             return reject(error);
           } else {
-            resolve(paymentResponse.body);
+            resolve(response.body);
           }
         });
-    }
+    };
+  },
+
+  getPayment: function(id) {
+    var _this = this;
+
+    return new Promise(resolve, reject) {
+      return http
+        .get(_this.url + '/sepa_credit_transfers/' + id)
+        .end(function(error, response) {
+          if (error) {
+            return reject(error)
+          } else {
+            resolve(response.body)
+          }
+        });
+    };
   }
 }
 
