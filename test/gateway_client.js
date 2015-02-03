4ee1df767e9a8db7e78cd7f71085b2c4919afeab
Yong-Soo Chung <yongsoochung@gmail.com>
2015-02-03 12:19:10 -0800

    [FEATURE] Gateway Client: createExternalTransaction, updateTransactionStatus

--- test/gateway_client.js
+++ test/gateway_client.js
@@ -2,11 +2,12 @@
 var GatewayClient = require('./../src/gateway_client')
 var assert = require('assert')
 var config = require('./../src/config')
+var uuid = require('uuid') 
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 describe('Gateway Client', function() {
-  var gatwayClient;
+  var gatewayClient;
 
   it('should initialize with gateway credentials', function() {
     gatewayClient = new GatewayClient({
@@ -23,6 +24,70 @@ describe('Gateway Client', function() {
       done();
     })
     .error(function(error) {
+      console.log('Error: ', error);
+      assert(!error);
+      done();
+    })
+  });
+
+  it('should be able to create a transaction', function(done) {
+    var uuidNum = uuid.v4();
+    var transaction = {
+      deposit: false,
+      external_account_id: 12,
+      source_amount: 2,
+      source_currency: 'EUR',
+      destination_amount: 2,
+      destination_currency: 'EUR',
+      status: 'invoiced',
+      ripple_transaction_id: 234,
+      uid: uuidNum,
+      invoice_id: '84cfb783192e9e563bf8e3b0ac31d9403a185e5af5f5a441db855dcc4a8a171a'
+    };
+
+    gatewayClient.createExternalTransaction(transaction)
+    .then(function(response) {
+      assert.strictEqual(response.success, true);
+      assert.strictEqual(response.externalTransaction.uid, uuidNum);
+      done()
+    })
+    .error(function(error) {
+      console.log('Error: ', error);
+      assert(!error);
+      done();
+    })
+  })
+
+  it('should be able to update transaction status to cleared', function(done) {
+    var transaction = {
+      deposit: false,
+      external_account_id: 12,
+      source_amount: 2,
+      source_currency: 'EUR',
+      destination_amount: 2,
+      destination_currency: 'EUR',
+      status: 'invoiced',
+      ripple_transaction_id: 234,
+      uid: uuid.v4(),
+      invoice_id: '84cfb783192e9e563bf8e3b0ac31d9403a185e5af5f5a441db855dcc4a8a171a'
+    };
+
+    gatewayClient.createExternalTransaction(transaction)
+    .then(function(transactions) {
+      console.log('transactions: ', transactions);
+      gatewayClient.updateTransactionStatus(transactions.externalTransaction.id, 'cleared')
+      .then(function(response) {
+        console.log('response is: ', response);
+        assert.strictEqual(response.externalTransaction.status, 'cleared');
+        done();
+      })
+      .error(function(error) {
+        console.log('Error: ', error);
+        assert(!error);
+        done();
+      })
+    })
+    .catch(function(error) {
       console.log('Error: ', error);
       assert(!error);
       done();
